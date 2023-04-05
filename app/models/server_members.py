16ac25ac2176c641e393988723369e032c84a732
Henry <henrytam95@gmail.com>
2023-04-04 19:40:27 -0700

    changes for render

--- app/models/server_members.py
+++ app/models/server_members.py
@@ -7,10 +7,8 @@ class ServerMember(db.Model):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey(
-        add_prefix_for_prod("users.id")), nullable=False)
-    server_id = db.Column(db.Integer, db.ForeignKey(
-        add_prefix_for_prod("servers.id")), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")), nullable=False)
+    server_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("servers.id")), nullable=False)
 
     user = db.relationship("User", back_populates="server_member")
     server = db.relationship("Server", back_populates="server_member")
