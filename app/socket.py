9320929a5777ff346059ac40d307a15e406bbed1
Henry <henrytam95@gmail.com>
2023-04-05 13:03:08 -0700

    trailing /

--- app/socket.py
+++ app/socket.py
@@ -5,7 +5,7 @@ import os
 # configure cors_allowed_origins
 if os.environ.get('FLASK_ENV') == 'production':
     origins = [
-        'https://tea-time-ayc5.onrender.com/'
+        'https://tea-time-ayc5.onrender.com'
         'http://tea-time-ayc5.onrender.com',
     ]
 else:
